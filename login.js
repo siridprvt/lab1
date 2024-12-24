ca5774a4bbe7f01d9c0700b5e3d68b1f5f5bd8b5
siridprvt <siridevojuprvt1@gmail.com>
2024-12-24 14:04:40 +0530

    updated login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function verify(){
     var un=document.getElementById("username").value;
     var pwd=document.getElementById("password").value;
     if(un==="cvr" && pwd==="cvr123"){
-        window.location.href="home.html";
+        window.location.href="home.html"
     }
     else{
         alert("Please enter valid credentials");
